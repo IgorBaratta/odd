46135ab2c7cfa4a49f00534613831be1844e030d
igorbaratta <igorbaratta@gmail.com>
2020-02-20 22:44:44 -0300

    add 1d test

--- test/test_assemble_matrix.py
+++ test/test_assemble_matrix.py
@@ -10,6 +10,7 @@ import numpy
 import odd
 import pytest
 from mpi4py import MPI
+from petsc4py import PETSc
 
 mesh_list = [dolfinx.UnitIntervalMesh(MPI.COMM_WORLD, 100),
              dolfinx.UnitSquareMesh(MPI.COMM_WORLD, 64, 64),
@@ -42,3 +43,57 @@ def test_assemble_matrix(mesh):
 
     assert (A-B).norm() < 1e-12
     assert (C-A).norm() < 1e-12
+
+
+def test_assemble_1d_bc():
+    """Solve -u’’ = sin(x), u(0)=0, u(L)=0."""
+
+    comm = MPI.COMM_WORLD
+    lim = [0.0, numpy.pi]
+    mesh = dolfinx.IntervalMesh(comm, 100, lim)
+    mesh.geometry.coord_mapping = dolfinx.fem.create_coordinate_map(mesh)
+    mesh.create_connectivity_all()
+    tdim = mesh.topology.dim
+
+    [x] = ufl.SpatialCoordinate(mesh.ufl_domain())
+    f = ufl.sin(x)
+
+    V = dolfinx.FunctionSpace(mesh, ("P", 2))
+    u = ufl.TrialFunction(V)
+    v = ufl.TestFunction(V)
+    a = ufl.inner(ufl.grad(u), ufl.grad(v)) * ufl.dx
+    L = ufl.inner(f, v) * ufl.dx
+
+    # Define boundary condition on x = lim[0] or x = lim[1]
+    u0 = dolfinx.Function(V)
+    u0.vector.set(0.0)
+    facets = numpy.where(mesh.topology.on_boundary(tdim-1))[0]
+    dofs = dolfinx.fem.locate_dofs_topological(V, tdim-1, facets)
+    values = numpy.zeros(dofs.size)
+
+    # Create matrix
+    A = odd.create_matrix(a)
+    odd.assemble_matrix(a, A)
+    A.assemble()
+    odd.apply_bc(A, dofs)
+
+    b = odd.assemble_vector(L)
+    odd.apply_bc(b, dofs, values)
+
+    ksp = PETSc.KSP().create(comm)
+    ksp.setOperators(A)
+    ksp.setType('preonly')
+    ksp.pc.setType('lu')
+    ksp.setFromOptions()
+
+    u = dolfinx.Function(V)
+    x = u.vector
+    ksp.solve(b, x)
+
+    u_e = dolfinx.Function(V)
+    u_e.interpolate(lambda x: numpy.sin(x[0]))
+
+    diff = u - u_e
+    e = dolfinx.fem.assemble_scalar(ufl.inner(diff, diff)*ufl.dx)
+    print(abs(e))
+    assert e == pytest.approx(0)
